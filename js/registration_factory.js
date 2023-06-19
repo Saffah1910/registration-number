b76a37e43643f0ab865fb8088bef133a89a01070
Saffah Khatib <skhatib1910@gmail.com>
2023-06-19 13:50:48 +0200

    changes

--- js/registration_factory.js
+++ js/registration_factory.js
@@ -1,44 +1,42 @@
 function RegistrationNumbers(regNo) {
-    var allTheRegNums = {};
+    var allTheRegNums = [];
 
 
 
     function validData(regNo) {
         if (regNo !== "") {
-            let regEx = /[\^C][A,J,L,F]\s?\d{1,3}\s?\d{1,3}$/
-            regEx.test(regNo);
-            return regNo.toUpperCase()
+          
+            allTheRegNums.push(regNo)
+            return allTheRegNums;
         }
-        else {
-            return "Enter registration number"
-        }
+       
     };
-
-    function invalidMessage(regNo) {
-        if (validData(regNo) == false) {
-
-        }
-        return "invalid data"
-    };
-
-
-    function regNumExists(regNo) {
-
-        if (allTheRegNums[regNo] === undefined) {
-            allTheRegNums[regNo] = 1
+    function addRed(){
+        if(regNo === ""){
+            return "red"
         }
     }
 
-    function keepRegNUm() {
-        if (validData()) {
-            var newList = Object.keys(allTheRegNums)
-            return newList;
-        }
-    };
 
-    function getRegNum() {
-        return allTheRegNums
-    };
+
+
+    // function regNumExists(regNo) {
+
+    //     if (allTheRegNums[regNo] === undefined) {
+    //         allTheRegNums[regNo] = 1
+    //     }
+    // }
+
+    // function keepRegNUm() {
+    //     if (validData()) {
+    //         var newList = Object.keys(allTheRegNums)
+    //         return newList;
+    //     }
+    // };
+
+    // function getRegNum() {
+    //     return allTheRegNums
+    // };
 
 
 
@@ -46,12 +44,72 @@ function RegistrationNumbers(regNo) {
     return {
 
         validData,
-        invalidMessage,
-        regNumExists,
-        keepRegNUm,
-        getRegNum
+        addRed
+        // keepRegNUm,
+        // getRegNum
 
     }
 
 
 }
+
+
+// function RegistrationNumbers() {
+//     // var allTheRegNums = {};
+
+// // var regNo = "";
+
+//     // function validData(regNo) {
+
+//     //     let regEx = /[\^C][A,J,L,F]\s?\d{1,3}\s?\d{1,3}$/
+//     //     return regEx.test(regNo)
+//     // };
+//     // function addRegNo(regNo){
+//     //     if(validData() === true && regNo !== ""){
+//     //         return regNo
+//     //     }
+
+//     // };
+
+
+
+
+
+
+// return{
+//     validData,
+//     addRegNo
+// }
+
+
+
+
+// }
+
+
+
+    // function invalidMessage(regNo) {
+    //     if (regEx.test(regNo) == false) {
+
+    //     }
+    //     return "invalid data"
+    // };
+
+
+    // function regNumExists(regNo) {
+
+    //     if (allTheRegNums[regNo] === undefined) {
+    //         allTheRegNums[regNo] = 1
+    //     }
+    // }
+
+    // function keepRegNUm() {
+    //     if (validData()) {
+    //         var newList = Object.keys(allTheRegNums)
+    //         return newList;
+    //     }
+    // };
+
+    // function getRegNum() {
+    //     return allTheRegNums
+    // };
--- js/registration_number.js
+++ js/registration_number.js
@@ -4,32 +4,52 @@ let boxRegNoElem = document.querySelector("#boxRegNo");
 let regNumsElem = document.querySelector(".regNums");
 
 
+
 addRegNoBtnElem.addEventListener("click", function () {
 
     var registrationNums = RegistrationNumbers();
 
-    // var valid = registrationNums.validData();
-
-    registrationNums.validData(textAreaElem.value);
-    //creates a new div
-    const newDiv = document.createElement("p");
-    //gives it content
-    const newContent = document.createTextNode(textAreaElem.value.toUpperCase());
-    //add content to new div
-    newDiv.appendChild(newContent);
-    //add element + content to DOM
-    const currentDiv = document.getElementById("boxRegNo");
-    document.body.insertBefore(newDiv, currentDiv);
+    let regEx = /[\^C][A,J,L,F]\s?\d{1,3}\s?\d{1,3}$/
+    let result = textAreaElem.value.match(regEx);
 
 
-    // var invalid = registrationNums.invalidMessage()
-    // if(invalid){
-    //     boxRegNoElem.innerHTML = registrationNums.invalidMessage()
-    // }
+    if (result && textAreaElem.value !== "") {
+        registrationNums.validData(textAreaElem.value);
+        //creates a new div
+        const newDiv = document.createElement("div");
+        //gives it content
+        const newContent = document.createTextNode(textAreaElem.value.toUpperCase());
+        //add content to new div
+        newDiv.appendChild(newContent);
+        //add element + content to DOM
+        const currentDiv = document.getElementById("boxRegNo");
+        document.body.insertBefore(newDiv, currentDiv);
+    }
+  
+    if (!result && textAreaElem.value !== "") {
+        registrationNums.validData(textAreaElem.value);
+        //creates a new div
+        const newDiv = document.createElement("div");
+        //gives it content
+        const newContent = document.createTextNode("Enter Valid data");
+        //add content to new div
+        newDiv.appendChild(newContent);
+        //add element + content to DOM
+        const currentDiv = document.getElementById("boxRegNo");
+        document.body.insertBefore(newDiv, currentDiv);
+    }
+    if(textAreaElem.value === ""){
+        const newDiv = document.createElement("div");
+        //gives it content
+        const newContent = document.createTextNode("no registatrion number entered");
+        //add content to new div
+        newDiv.appendChild(newContent);
+        //add element + content to DOM
+        const currentDiv = document.getElementById("boxRegNo");
+        document.body.insertBefore(newDiv, currentDiv); 
+    }
+
+});
 
 
 
-
-
-
-})
